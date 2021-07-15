173540d7cf240718b97287ddd597c489f8cf3057
jeffnoronha777 <62403545+jeffnoronha777@users.noreply.github.com>
2021-07-15 11:59:31 -0300

    Adicionado api do PHPMailer

--- web/js/script.js
+++ web/js/script.js
@@ -1,16 +1,26 @@
-if (window.SimpleSlide){
-new SimpleSlide({
-  slide: 'quote', // nome do atributo data-slide="principal"
-  time: 5000, // tempo de transição dos slides
-});
+if (window.SimpleSlide) {
+  new SimpleSlide({
+    slide: 'quote', // nome do atributo data-slide="principal"
+    time: 5000 // tempo de transição dos slides
+  })
 
-new SimpleSlide({
-  slide: 'portfolio', // nome do atributo data-slide="principal"
-  time: 5000, // tempo de transição dos slides
-  nav: true
-});
+  new SimpleSlide({
+    slide: 'portfolio', // nome do atributo data-slide="principal"
+    time: 5000, // tempo de transição dos slides
+    nav: true
+  })
 }
 
-if(window.SimpleAnime){
-  new SimpleAnime();
+if (window.SimpleAnime) {
+  new SimpleAnime()
+}
+
+if (window.SimpleForm) {
+  new SimpleForm({
+    form: '.formphp',
+    button: '#enviar',
+    erro: "<div id='form-erro'><h2>Erro no envio!!!</h2><p>Um erro ocorreu, tente para o email contato@bikcraft.com.br.</p></div>",
+    sucesso:
+      "<div id='form-sucesso'><h2>Formulário enviado com sucesso</h2><p>Em breve eu entro em contato com você.</p>"
+  })
 }
